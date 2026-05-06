ac631dccc75d3a47a28ecdb2f17c6f2b4f2d401d
SMK <60331327+stewart2005@users.noreply.github.com>
2026-05-05 19:26:59 -0600

    Add files via upload

---
+++ bases.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clash Of Build</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+
+
+    <?php include_once 'include/header_include.php';?>
+
+
+
+    <main>
+        <section class="jumbotron-selector">
+            <h2>Selecciona tu ayuntamiento</h2>
+            <p>Explora diseños estrategicos desde TH3 asta TH18</p>
+            <div class="th-grid">
+                <div class="th-card">
+                    <img src="img/ths/th_3.png" alt="TH 3">
+                    <button class="btn-th">Ayuntamiento LVL3</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/ths/th_4.png" alt="TH 4">
+                    <button class="btn-th">Ayuntamiento LVL4</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/ths/th_5.png" alt="TH 5">
+                    <button class="btn-th">Ayuntamiento LVL5</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/ths/th_6.png" alt="TH 6">
+                    <button class="btn-th">Ayuntamiento LVL6</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/ths/th_7.png" alt="TH 7">
+                    <button class="btn-th">Ayuntamiento LVL7</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/ths/th_8.png" alt="TH 8">
+                    <button class="btn-th">Ayuntamiento LVL8</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/ths/th_9.png" alt="TH 9">
+                    <button class="btn-th">Ayuntamiento LVL9</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/ths/th_10.png" alt="TH 10">
+                    <button class="btn-th">Ayuntamiento LVL10</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/ths/th_11.png" alt="TH 11">
+                    <button class="btn-th">Ayuntamiento LVL11</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/ths/th_12.png" alt="TH 12">
+                    <button class="btn-th">Ayuntamiento LVL12</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/ths/th_13.png" alt="TH 13">
+                    <button class="btn-th">Ayuntamiento LVL13</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/ths/th_14.png" alt="TH 14">
+                    <button class="btn-th">Ayuntamiento LVL14</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/ths/th_15.png" alt="TH 15">
+                    <button class="btn-th">Ayuntamiento LVL15</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/ths/th_16.png" alt="TH 16">
+                    <button class="btn-th">Ayuntamiento LVL16</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/ths/th_17.png" alt="TH 17">
+                    <button class="btn-th">Ayuntamiento LVL17</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/ths/th_18.png" alt="TH 18">
+                    <button class="btn-th">Ayuntamiento LVL18</button>
+                </div>
+            </div>
+            <h2>Selecciona tu taller del constructor</h2>
+            <p>Explora diseños estrategicos desde BH3 asta BH 10</p>
+            <div class="th-grid">
+                <div class="th-card">
+                    <img src="img/bhs/bh_3.png" alt="BH 3">
+                    <button class="btn-th">Taller LVL3</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/bhs/bh_4.png" alt="BH 4">
+                    <button class="btn-th">Taller LVL4</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/bhs/bh_5.png" alt="BH 5">
+                    <button class="btn-th">Taller LVL5</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/bhs/bh_6.png" alt="BH 6">
+                    <button class="btn-th">Taller LVL6</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/bhs/bh_7.png" alt="BH 7">
+                    <button class="btn-th">Taller LVL7</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/bhs/bh_8.png" alt="BH 8">
+                    <button class="btn-th">Taller LVL8</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/bhs/bh_9.png" alt="BH 9">
+                    <button class="btn-th">Taller LVL9</button>
+                </div>
+                <div class="th-card">
+                    <img src="img/bhs/bh_10.png" alt="BH 10">
+                    <button class="btn-th">Taller LVL10</button>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+
+<?php include_once 'include/footer_include.php';?>
+
+</html>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clash Of Build</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <?php include_once 'include/header_include.php'; ?>
+
+    <main>
+        <section class="jumbotron">
+            <h2>Bienvenido jefe!!!</h2>
+            <p>
+                Esta pagina fue creada con el objetivo de ayudar a jugadores nuevos o veteranos, intentaremos crear una
+                comunidad que nos ayude a subir bases, informacion o guias de Clash Of Clans para poder crecer juntos.
+            </p>
+        </section>
+
+        <section class="jumbotron-updates">
+            <h3><img src="img/icons/Builder.png" alt="" class="icon-scale">Update - 3/5/26</h3>
+            <ul>
+                <li><img src="img/icons/hammer.png" alt="" class="icon-scale">Interfaz principal y primeras ideas</li>
+            </ul>
+        </section>
+    </main>
+
+    <?php include_once 'include/footer_include.php'; ?>
+
+</body>
+
+</html>
